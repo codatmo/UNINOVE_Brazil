c373254f81b593ffaa11631e2ab0ec0fef31f4bc
breckbaldwin <git@lingpipe.com>
2021-04-26 06:57:49 -0400

    got held out data working

--- anno-model/R/classifier.R
+++ anno-model/R/classifier.R
@@ -1,14 +1,15 @@
 library(cmdstanr)
-#library(rstan)
+# Description of this code is at: 
+# https://github.com/codatmo/UNINOVE_Sao_Paulo/tree/main/anno-model/TweetClassifier.html
 
-d <- read.csv("../data/long_format.csv")
+d <- read.csv("../data/long_format.csv") # Not included with code due to IP restrictions
 
 d1 <- d[d$annotator_id==1,]
 d2 <- d[d$annotator_id==2,]
 d3 <- d[d$annotator_id==3,]
 
 cd2 <- d2
-cd2$label_id <- ifelse(cd2$label_id > 2, 1, cd2$label_id) # collapse 3-8 to 2
+cd2$label_id <- ifelse(cd2$label_id > 2, 1, cd2$label_id) # collapse annotations 3-8 to 2
 cd2$label_id <- cd2$label_id - 1 # map 2 to 1, 1 to 0
 # split on white space
 cd2$words <- strsplit(as.character(cd2$text),"\\s+", perl = TRUE)
@@ -44,24 +45,38 @@ words2binary_vector <- function(words, vocab_lookup) {
   return(sent_vect)
 }
 
-binary_pred_vector <- matrix(nrow = nrow(cd2), 
+binary_pred_matrix <- matrix(nrow = nrow(cd2), 
        ncol = index)
 
 for (i in 1:nrow(cd2)) {
-  binary_pred_vector[i,] <- words2binary_vector(cd2[i,]$words, vocab_lookup = vocab)
+  binary_pred_matrix[i,] <- words2binary_vector(cd2[i,]$words, vocab_lookup = vocab)
 }
 
-stan_data <- list(N_data_count = nrow(binary_pred_vector), 
-                  M_feature_count = ncol(binary_pred_vector),
-                  x_features = binary_pred_vector, 
-                  y_categories = cd2$label_id)
+N_data_count <- nrow(binary_pred_matrix)
+held_out <- 100
+stan_data <- list(N_data_count = N_data_count - held_out + 1, 
+                  M_feature_count = ncol(binary_pred_matrix),
+                  H_held_out = held_out,
+                  x_features = binary_pred_matrix[held_out:N_data_count,], 
+                  y_categories = cd2[held_out:N_data_count,]$label_id,
+                  x_held_out = binary_pred_matrix[1:held_out,],
+                  y_held_out = cd2[1:held_out,]$label_id)
 
 model <- cmdstan_model("../stan/classifier.stan")
 
-#fit <- model$sample(data = stan_data, seed = 22, chains = 1)
+fit <- model$sample(data = stan_data, seed = 22, chains = 4, 
+                    iter_warmup = 1000, iter_sampling = 1000)
 
-#fit$save_object("one_chain.RDS")
+# evaluating on training data, not a good idea
+print(fit$draws(c('precision', 'recall')))
 
-library(bayesplot)
+fit$save_object("held_out1.RDS")
 
-fit_mle <- model$optimize(data = stan_data, seed = 22)
+library(shinystan)
+library(rstan)
+
+stanfit <- rstan::read_stan_csv(fit$output_files())
+launch_shinystan(stanfit)
+
+
+# fit_mle <- model$optimize(data = stan_data, seed = 22)
--- anno-model/stan/classifier.stan
+++ anno-model/stan/classifier.stan
@@ -1,9 +1,12 @@
 data {
   int N_data_count;
   int M_feature_count;
-  //row_vector[M_feature_count] x_features[N_data_count];
+  int H_held_out;
+  // row_vector[N_data_count] x_features[M_feature_count];
   matrix [N_data_count,M_feature_count] x_features;
+  matrix [H_held_out,M_feature_count] x_held_out;
   int y_categories[N_data_count];
+  int y_held_out[H_held_out];
 }
 
 parameters {
@@ -14,5 +17,40 @@ parameters {
 model {
   intercept ~ normal(0,2);
   b_coefs ~ normal(0,2);
-  y_categories ~ bernoulli_logit(x_features * b_coefs + intercept);
+  // y_categories ~ bernoulli_logit(x_features * b_coefs + intercept);
+  target += bernoulli_logit_glm_lpmf(y_categories | x_features, intercept, b_coefs);
+}
+
+generated quantities {
+  real precision;
+  real recall;
+  real tp = 0;
+  real fp = 0;
+  real tn = 0;
+  real fn = 0;
+
+  for (i in 1:H_held_out) {
+    real prob = inv_logit(x_held_out[i] * b_coefs + intercept);
+    print("prob=", prob);
+    if (prob > .5) { 
+      if (y_held_out[i] == 1) {
+        tp += 1;
+      }
+      else {
+        fp += 1;
+      }
+    }
+    else {
+      if (y_held_out[i] == 1) {
+        fn += 1;
+      }
+      else {
+        tn += 1;
+      }
+    }
+  }
+  precision = tp/(tp + fp);
+  recall = tp/(tp + fn);
+  print("precision=", precision);
+  print("recall=", recall);
 }
