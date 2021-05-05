ceb1fc0940258e9e289d984d38843ab3b80963b7
Jose Eduardo Storopoli <thestoropoli@gmail.com>
2021-05-05 13:15:12 -0300

    Better handling of data and updating data for 05th May 2021.

--- SEIR-model/R/get-data.R
+++ SEIR-model/R/get-data.R
@@ -1,32 +1,15 @@
-# Based on Andre's code: https://github.com/andrelmfsantos/Brazil_IO/blob/main/Brasil_IO_API.Rmd
 # API doc can be found here: https://github.com/turicas/covid19-br/blob/master/api.md
 
 library(dplyr)
 library(lubridate)
+library(readr)
 
-download_brasilio_table <- function(dataset, table_name){
-  url <- sprintf("https://data.brasil.io/dataset/%s/%s.csv.gz", dataset, table_name)
-  tmp <- tempfile()
-  download.file(url, tmp)
-  response <- read.csv(gzfile(tmp), encoding = "UTF-8")
-  unlink(tmp)
-  return(response)
-}
+data <- read_csv("https://data.brasil.io/dataset/covid19/caso_full.csv.gz")
 
-# table name is one of "caso", "caso_full", "obito_cartorio"
-# we are choosing "caso_full" for the full data
-
-data <- download_brasilio_table("covid19", "caso_full") # 42mb
-
-# Getting only 2021
-y21 <- data %>%
-  as_tibble %>% 
-  mutate(date = as_date(date)) %>% 
-  filter(date <= max(date) & date >= "2021-01-01")
 
 # Getting only state stuff
-state <- y21 %>% 
-  filter(city == "") %>% 
+state <- data %>% 
+  filter(is.na(city)) %>% 
   select(-city)
 
 # Getting Brazil's national-level data
@@ -41,5 +24,5 @@ br <- state %>%
   )
 
 # save file
-saveRDS(state, here::here("SEIR-model", "data", "state_2021.rds"))
-saveRDS(br, here::here("SEIR-model", "data", "brazil_2021.rds"))
+saveRDS(state, here::here("SEIR-model", "data", "brazil_state.rds"))
+saveRDS(br, here::here("SEIR-model", "data", "brazil_nation.rds"))
