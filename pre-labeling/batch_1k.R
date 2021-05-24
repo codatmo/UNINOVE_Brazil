3195d34a2b5afdea2ae4867b313c767127baabcd
Jose Eduardo Storopoli <thestoropoli@gmail.com>
2021-05-24 18:43:21 -0300

    round 5 batch_1k

--- pre-labeling/batch_1k.R
+++ pre-labeling/batch_1k.R
@@ -28,28 +28,38 @@ sample_df <- df %>%
 
 # Second round of 1k cross-anotation
 set.seed(124)
-sample_df2 <- df %>% 
-  drop_na(text) %>% 
-  sample_n(1e3) %>% 
+sample_df2 <- df %>%
+  drop_na(text) %>%
+  sample_n(1e3) %>%
   anti_join(sample_df)
 
-# 100 samples to teach annotations
+# 100 samples to teach anotations
 set.seed(125)
-sample_df3 <- df %>% 
-  drop_na(text) %>% 
-  sample_n(1e2) %>% 
-  anti_join(sample_df) %>% 
+sample_df3 <- df %>%
+  drop_na(text) %>%
+  sample_n(1e2) %>%
+  anti_join(sample_df) %>%
   anti_join(sample_df2)
 
 # Third round of 1k anotation
 set.seed(126)
-sample_df4 <- df %>% 
-  drop_na(text) %>% 
-  sample_n(4002) %>% 
-  anti_join(sample_df) %>% 
-  anti_join(sample_df2) %>% 
+sample_df4 <- df %>%
+  drop_na(text) %>%
+  sample_n(4002) %>%
+  anti_join(sample_df) %>%
+  anti_join(sample_df2) %>%
   anti_join(sample_df3)
+
+# Fourth round of 1k anotation
+set.seed(127)
+sample_df5 <- df %>%
+  drop_na(text) %>%
+  sample_n(1e3) %>%
+  anti_join(sample_df) %>%
+  anti_join(sample_df2) %>%
+  anti_join(sample_df3) %>%
+  anti_join(sample_df4)
 
 file_delete(file$local_path)
 
-write_csv(sample_df4, "sample_4k.csv")
+write_csv(sample_df5, "sample_1k.csv")
